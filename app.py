ae673d20eac20f5b4efe0ff1c1abf00f65c15dd4
Vanqi <143060834+hopecommon@users.noreply.github.com>
2024-04-22 10:27:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,17 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, AutoModel
 from openxlab.model import download
 
 base_path = './try'
-os.system('apt install git')
-os.system('apt install git-lfs')
-os.system(f'git clone https://code.openxlab.org.cn/hopecommon/try.git {base_path}')
-os.system(f'cd {base_path} && git lfs pull')
+if not os.path.exists(base_path):
+    os.system('apt install git')
+    os.system('apt install git-lfs')
+    os.system("git lfs install")
+    os.system(f'git clone https://code.openxlab.org.cn/hopecommon/try.git {base_path}')
+    os.system("git lfs install")
+    os.system(f'cd {base_path} && git lfs pull')
 tokenizer = AutoTokenizer.from_pretrained(base_path,trust_remote_code=True)
 model = AutoModelForCausalLM.from_pretrained(base_path,trust_remote_code=True, torch_dtype=torch.float16).cuda()
+
+
 
 def chat(message,history):
     for response,history in model.stream_chat(tokenizer,message,history,max_length=2048,top_p=0.7,temperature=1):
