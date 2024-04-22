89f09924ec9ec3e6b0e2b876be237b5adfd0d4fe
Vanqi <143060834+hopecommon@users.noreply.github.com>
2024-04-22 09:51:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer, AutoModel
 from openxlab.model import download
 
-base_path = './internlm2-chat-7b'
-os.system(f'git clone https://code.openxlab.org.cn/OpenLMLab/internlm2-chat-7b.git {base_path}')
+base_path = './try'
+os.system('apt install git')
+os.system('apt install git-lfs')
+os.system(f'git clone https://code.openxlab.org.cn/hopecommon/try.git {base_path}')
 os.system(f'cd {base_path} && git lfs pull')
-
 tokenizer = AutoTokenizer.from_pretrained(base_path,trust_remote_code=True)
 model = AutoModelForCausalLM.from_pretrained(base_path,trust_remote_code=True, torch_dtype=torch.float16).cuda()
 
