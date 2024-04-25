ccb204d3e3fb0edb423d7271ba73a37a6465cc4e
Vanqi <143060834+hopecommon@users.noreply.github.com>
2024-04-25 17:09:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, AutoModel
 base_path = './personal_assistant'
 os.system('apt install git')
 os.system('apt install git-lfs')
-os.system(f'git clone https://code.openxlab.org.cn/hopecommon/personal_assistant.git {base_path}')
+os.system(f'git clone https://code.openxlab.org.cn/hopecommon/try.git {base_path}')
 os.system(f'cd {base_path} && git lfs pull')
 tokenizer = AutoTokenizer.from_pretrained(base_path,trust_remote_code=True)
 model = AutoModelForCausalLM.from_pretrained(base_path,trust_remote_code=True, torch_dtype=torch.float16).cuda()
